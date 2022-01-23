e7e1dd99eb3d492d4b984ce528523ee0d9f38907
Alex Dutton <aed1988@gmail.com>
2022-01-23 19:51:05 +0000

    Completed some SQL katas

---
+++ SQL/register_for_the_party.sql
@@ -0,0 +1,18 @@
+-- Register for the Party
+
+-- You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+
+-- participants table schema
+
+-- name (string)
+-- age (integer)
+-- attending (boolean)
+-- NOTES:
+
+-- Since alcohol will be served, you can only attend if you are 21 or older
+-- You can't attend if the attending column returns anything but true
+
+
+INSERT INTO participants VALUES ('Alex Dutton', '33', true);
+
+SELECT * FROM participants;
