bf05e02518e7b652f21294897350ceeb44eb8e92
Alex Dutton <aed1988@gmail.com>
2022-01-24 20:20:09 +0000

    More SQL katas completed

---
+++ SQL/returning_strings.sql
@@ -0,0 +1,5 @@
+-- Write a select statement that takes name from person table and return 
+-- "Hello, <name> how are you doing today?" results in a column named greeting
+
+SELECT ('Hello, ' || name || ' how are you doing today?') AS greeting
+FROM person
